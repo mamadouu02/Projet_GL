0d3bc8adc650ecd9640ae6819d5c83efb09720b9
AHJAOU <samy.ahjaou@grenoble-inp.fr>
2024-01-12 12:49:32 +0100

    Suite context: fin de la partie decl var

--- src/main/java/fr/ensimag/deca/tree/IntLiteral.java
+++ src/main/java/fr/ensimag/deca/tree/IntLiteral.java
@@ -28,7 +28,10 @@ public class IntLiteral extends AbstractExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        //throw new UnsupportedOperationException("not yet implemented");
+        Type type = compiler.environmentType.INT;
+        setType(type);
+        return type;
     }
 
 
