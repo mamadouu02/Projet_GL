210a0e0d7b52b72a93b5d7f46a870e946880e5b5
Hamza Bouihi <Hamza.Bouihi@grenoble-inp.org>
2024-01-15 11:24:37 +0100

    initialisation negatif

--- src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
+++ src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
@@ -1,6 +1,13 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.context.Type;
+import fr.ensimag.ima.pseudocode.ImmediateFloat;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.POP;
+import fr.ensimag.ima.pseudocode.instructions.PUSH;
+import fr.ensimag.ima.pseudocode.instructions.SUB;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
@@ -34,4 +41,16 @@ public class UnaryMinus extends AbstractUnaryExpr {
         return "-";
     }
 
+
+    @Override
+    protected void codeGenExpr(DecacCompiler compiler) {
+            if (getType().isFloat()) {
+                compiler.addInstruction(new LOAD(new ImmediateFloat(0), Register.getR(compiler.getIdReg())));
+            } else {
+                compiler.addInstruction(new LOAD(new ImmediateInteger(0), Register.getR(compiler.getIdReg())));
+            }
+
+            compiler.addInstruction(new SUB(getOperand().dVal(), Register.getR(compiler.getIdReg())));
+
+    }
 }
