247d662d99d741b07351dc42045cc7d065513dd0
Grégoire Sename <gregoire.sename@grenoble-inp.org>
2024-01-26 10:18:38 +0100

    feat(trigo): file test imperatif, correction classe et demo

---
+++ demo/extension/compare_math.py
@@ -0,0 +1,28 @@
+import subprocess
+import math
+
+def lire_valeurs_de_sortie_deca():
+    process = subprocess.Popen(["ima", "./test_include_math.ass"], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    output = output.decode("utf-8")  # Décodez les bytes en str
+    valeurs = [float(ligne.strip()) for ligne in output.split('\n') if ligne.strip()]
+    return valeurs
+
+def comparer_valeurs(valeurs_deca):
+
+    valeurs_math = []
+    for i in range(10):
+        valeurs_math.append(math.sin(4 * math.pi /(i+1)));    
+    # for i in range(0, 10):
+    #     valeurs_math.append(math.atan(-(i /100.0 + 0.85)));
+    
+    # Comparer les valeurs
+    for i in range(10):
+        # print(valeurs_deca[i])
+        # print(valeurs_math[i])
+        print(abs(valeurs_deca[i] - valeurs_math[i]))
+
+if __name__ == "__main__":
+    valeurs_deca = lire_valeurs_de_sortie_deca()
+    comparer_valeurs(valeurs_deca)
+
